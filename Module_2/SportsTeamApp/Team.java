cdddadc6433b2248ee4544348cf909fd1f7c8f7d
rbecthold <rbecthold@my365.bellevue.edu>
2021-09-11 14:03:19 -0500

    Completed 2.3 SportsTeamApp assignment

--- Module_2/SportsTeamApp/Team.java
+++ Module_2/SportsTeamApp/Team.java
@@ -1,36 +1,63 @@
-/**
- * Citations and comments to come.
- * Need to see results of week 1 assignment first.
- */
+/*
+    Becthold, R. (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+
+    Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+*/
 
  public class Team {
 
-    //Defining Team class attributes
+    /**
+     * Creating the following private data fields to represent the team's attributes:
+     *     teamName    -> represents the name of the team
+     *     players     -> represents the names of the team's players
+     *     playerCount -> represents the number of players on the team
+     */
     private String teamName;
     private String[] players;
     private int playerCount;
     
+    /**
+     * Constructor method with 1 argument, which returns a Fan object with the given attributes.
+     * @param teamName String
+     * @return gives a Team object with the specified name
+     */
     public Team(String teamName) {
         this.teamName = teamName;
         this.players = new String[20];
         this.playerCount = 0;
     } //end Team() argument constructor
 
-    public void addPlayer(String player) {
-        players[playerCount] = player;
-        playerCount++;
-    } //end addPlayer method
-
+    /**
+     * GET method for the 'players' attribute
+     * @return Returns the current value of the 'players' attribute.
+     */
     public String[] getPlayers() {
         return players;
     } //end getPlayers method
 
+    /**
+     * GET method for the 'playerCount' attribute
+     * @return Returns the current value of the 'playerCount' attribute.
+     */
     public int getPlayerCount() {
         return playerCount;
     } //end getPlayerCount method
 
+    /**
+     * GET method for the 'teamName' attribute
+     * @return Returns the current value of the 'teamName' attribute.
+     */
     public String getTeamName() {
         return teamName;
     } //end getTeamName method
+
+    /**
+     * Void addPlayer method, appends the String player name to the 'players' array.
+     * @param player
+     */
+    public void addPlayer(String player) {
+        players[playerCount] = player;
+        playerCount++;
+    } //end addPlayer method
 
  } //end Team class
--- Module_2/SportsTeamApp/TestSportsTeamApp.java
+++ Module_2/SportsTeamApp/TestSportsTeamApp.java
@@ -1,7 +1,11 @@
-/**
- * Citations and comments to come.
- * Need to see results of week 1 assignment first.
- */
+/*
+    Becthold, R. (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+
+    Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+    Comprehensive Version (12th ed.). Pearson Education, Inc.
+*/
 
 import java.util.Scanner;
 
@@ -9,41 +13,62 @@ public class TestSportsTeamApp {
     
     public static void main(String[] args) {
 
+        // Initialize a new Scanner object for user input
         Scanner input = new Scanner(System.in);
 
         System.out.println("\n  Welcome to the Sports Team App\n");
-        System.out.print("  Enter a team name: ");
-        String teamName = input.nextLine();
 
-        System.out.println("\n  Enter the player names:");
-        System.out.print("    hint: use commas for multiple players; no spaces>: ");
-        String players = input.nextLine();
+        // while loop for adding multiple Teams
+        char continueDecision = 'y';
+        while(continueDecision == 'y') {
 
-        Team team = new Team(teamName);
-        String[] names = players.split(",");
-        for(int i = 0; i < names.length; i++) {
-            team.addPlayer(names[i]);
-        }
+            // get user input for teamName
+            System.out.print("  Enter a team name: ");
+            String teamName = input.nextLine();
 
-        System.out.println("\n  --Team Summary--");
-        System.out.printf("  Team name: %s\n", team.getTeamName());
-        System.out.printf("  Number of players in team: %d\n", team.getPlayerCount());
-        System.out.print("  Players on team: ");
-        for(int i = 0; i < team.getPlayerCount(); i++) {
-            System.out.print(team.getPlayers()[i]);
-            if(i+1 < team.getPlayerCount()) {
-                System.out.print(",");
+            // get user input for player names array
+            System.out.println("\n  Enter the player names:");
+            System.out.print("    hint: use commas for multiple players; no spaces>: ");
+            String players = input.nextLine();
+
+            // Initialize new Team object using the given attributes
+            Team team = new Team(teamName);
+            String[] names = players.split(",");
+            for(int nameIndex = 0; nameIndex < names.length; nameIndex++) {
+                team.addPlayer(names[nameIndex]);
+            }
+
+            // Print summary of Team information to console
+            System.out.println("\n  --Team Summary--");
+            System.out.printf("  Team name: %s\n", team.getTeamName());
+            System.out.printf("  Number of players in team: %d\n", team.getPlayerCount());
+            System.out.print("  Players on team: ");
+            for(int playerIndex = 0; playerIndex < team.getPlayerCount(); playerIndex++) {
+                System.out.print(team.getPlayers()[playerIndex]);
+                if(playerIndex + 1 < team.getPlayerCount()) {
+                    System.out.print(",");
+                }
+                else {
+                    System.out.println("\n");
+                }
+            }
+
+            // Ask user whether more Team objects need to be created
+            System.out.print("  Continue? (y/n): ");
+            continueDecision = input.nextLine().charAt(0);
+
+            // Loop again for a new team or break from the loop
+            if(continueDecision == 'y') {
+                System.out.println();
             }
             else {
-                System.out.println("\n");
+                System.out.println("\n\n  End of line...");
             }
         }
 
-        /*
-        TODO: Add in functionality to make the code loop so you can add (& display) multiple teams
-        */
-
+        // Close and release input Scanner object
         input.close();
-    }
+
+    }// end main
 
 } //end TestSportsTeamApp
